b84ea4db7e916d8db4cceddb6b7d0600c6995c1c
Buchanan Kerswell <buck.kerswell@gmail.com>
2021-02-01 08:35:02 -0700

    add read nodes function to visualize grid

--- functions.R
+++ functions.R
@@ -2,7 +2,19 @@
 cat('Loading packages\n')
 suppressMessages(
   lapply(
-    c('ggplot2', 'broom', 'dplyr', 'scales', 'patchwork', 'purrr', 'directlabels', 'readr'),
+    c('ggplot2',
+      'broom',
+      'dplyr',
+      'scales',
+      'patchwork',
+      'purrr',
+      'directlabels',
+      'readr',
+      'knitr',
+      'kableExtra',
+      'tidyr',
+      'progress',
+      'metR'),
     FUN = function(x) {
       if (!require(x, character.only = TRUE)) {
         install.packages(x, dependencies = TRUE)
@@ -215,4 +227,344 @@ m.lin <- function(data,
   } else{
     return(list(model = cf, fit = ft))
   }
+}
+
+# Read binary (.prn) files and save grids
+read_nodes <- function(dir, prn = seq(10, 15, 1)){
+  # List prn files
+  fnames <- list.files(dir, '*.prn', full.names = T)
+  f.order <- fnames %>%
+    purrr::map_int(~.x %>%
+                     stringr::str_extract('[0-9]+.prn+') %>%
+                     stringr::str_extract('[0-9]+') %>%
+                     as.integer()) %>% 
+    order()
+  fnames.ordered <- fnames[f.order]
+  if(max(prn) > length(fnames.ordered)){
+    stop('prn file too large: prns should be between 0 and 100')
+  }
+  # Read binary, save grid
+  grids <- purrr::map(prn, ~{
+    # Filename
+    fname <- fnames.ordered[.x]
+    # Open connection
+    f.prn <- file(fname, 'rb')
+    # Read sizes of variables
+    readBin(f.prn, 'integer', 4, 1, signed = F)
+    # Read model parameters
+    # Grid resolution
+    xnumx <- readBin(f.prn, 'integer', 1, 8)
+    znumz <- readBin(f.prn, 'integer', 1, 8)
+    # Markers per cell
+    mnumx <- readBin(f.prn, 'integer', 1, 8)
+    mnumz <- readBin(f.prn, 'integer', 1, 8)
+    # Number of markers
+    marknum <- readBin(f.prn, 'integer', 1, 8)
+    # Model sizes
+    xsize <- readBin(f.prn, 'numeric', 1, 8)
+    zsize <- readBin(f.prn, 'numeric', 1, 8)
+    # Pressure value
+    pinit <- readBin(f.prn, 'numeric', 5, 8)
+    # Gravity
+    gx <- readBin(f.prn, 'numeric', 1, 8)
+    gz <- readBin(f.prn, 'numeric', 1, 8)
+    # Number of rocks
+    rocknum <- readBin(f.prn, 'integer', 1, 4)
+    # Number of Boundary conditions
+    boundnum <- readBin(f.prn, 'integer', 1, 8)
+    # Stage, time
+    stg <- readBin(f.prn, 'integer', 1, 4)
+    timesum <- readBin(f.prn, 'numeric', 1, 8)
+    # Skip rock properties
+    curpos <- 4+2*4+16*8+rocknum*(8*24+4)
+    seek(f.prn, curpos, 'start')
+    # Initialize Matrices
+    pr <- matrix(NA, znumz, xnumx)
+    vx <- matrix(NA, znumz, xnumx)
+    vz <- matrix(NA, znumz, xnumx)
+    exx <- matrix(NA, znumz, xnumx)
+    ezz <- matrix(NA, znumz, xnumx)
+    exz <- matrix(NA, znumz, xnumx)
+    sxx <- matrix(NA, znumz, xnumx)
+    szz <- matrix(NA, znumz, xnumx)
+    sxz <- matrix(NA, znumz, xnumx)
+    ro <- matrix(NA, znumz, xnumx)
+    nu <- matrix(NA, znumz, xnumx)
+    nd <- matrix(NA, znumz, xnumx)
+    mu <- matrix(NA, znumz, xnumx)
+    ep <- matrix(NA, znumz, xnumx)
+    et <- matrix(NA, znumz, xnumx)
+    pr0 <- matrix(NA, znumz, xnumx)
+    prb <- matrix(NA, znumz, xnumx)
+    dv <- matrix(NA, znumz, xnumx)
+    tk <- matrix(NA, znumz, xnumx)
+    cp <- matrix(NA, znumz, xnumx)
+    kt <- matrix(NA, znumz, xnumx)
+    ht <- matrix(NA, znumz, xnumx)
+    eii <- matrix(1, znumz, xnumx)*1e-16
+    sii <- matrix(1, znumz, xnumx)*1e+4
+    # Progress bar
+    pb.nodes <- progress_bar$new(format = paste0('Reading Nodes [',
+                                                 stringr::str_extract(fname, 'cd.[0-9]{2}.[0-9]+'),
+                                                 '] [:bar] :percent in: :elapsed'),
+                                 total = xnumx*znumz,
+                                 clear = FALSE,
+                                 width = 60)
+    # Read nodes information
+    for(i in seq_len(xnumx)){
+      for(j in seq_len(znumz)){
+        vbuf <- readBin(f.prn, 'numeric', 3, 4)
+        pr[j,i] <- vbuf[1]
+        vx[j,i] <- vbuf[2]
+        vz[j,i] <- vbuf[3]
+        vbuf1 <- readBin(f.prn, 'integer', 3, 8)
+        vbuf2 <- readBin(f.prn, 'numeric', 16, 4)
+        exx[j,i] <- vbuf2[1]
+        ezz[j,i] <- vbuf2[2]
+        exz[j,i] <- vbuf2[3]
+        sxx[j,i] <- vbuf2[4]
+        szz[j,i] <- vbuf2[5]
+        sxz[j,i] <- vbuf2[6]
+        ro[j,i] <- vbuf2[7]
+        nu[j,i] <- vbuf2[8]
+        nd[j,i] <- vbuf2[9]
+        mu[j,i] <- vbuf2[10]
+        ep[j,i] <- vbuf2[11]
+        et[j,i] <- vbuf2[12]
+        pr0[j,i] <- vbuf2[13]
+        prb[j,i] <- vbuf2[14]
+        dv[j,i] <- vbuf2[15]
+        tk[j,i] <- vbuf2[16]
+        vbuf3 <- readBin(f.prn, 'integer', 1, 8)
+        vbuf4 <- readBin(f.prn, 'numeric', 3, 4)
+        cp[j,i] <- vbuf4[1]
+        kt[j,i] <- vbuf4[2]
+        ht[j,i] <- vbuf4[3]
+        pb.nodes$tick()
+      }
+    }
+    # Skip all nodes
+    curpos2 <- curpos+(4*22+8*4)*xnumx*znumz
+    seek(f.prn, curpos2, 'start')  
+    # Read gridline positions
+    gx <- readBin(f.prn, 'numeric', xnumx, 4)
+    gz <- readBin(f.prn, 'numeric', znumz, 4)
+    # Progress bar
+    pb.eii <- progress_bar$new(format = 'Calc. Stress & Strain [:bar] :percent in: :elapsed',
+                               total = (xnumx*znumz)-4,
+                               clear = FALSE,
+                               width = 60)
+    # Calculate stress & strain
+    for (i in seq_len(xnumx-2)){
+      for (j in seq_len(znumz-2)){
+        eii[j+1,i+1]=(exz[j+1,i+1]^2+((exx[j+1,i+1]+exx[j+2,i+1]+exx[j+1,i+2]+exx[j+2,i+2])/4)^2)^0.5;
+        sii[j+1,i+1]=(sxz[j+1,i+1]^2+((sxx[j+1,i+1]+sxx[j+2,i+1]+sxx[j+1,i+2]+sxx[j+2,i+2])/4)^2)^0.5;
+        pb.eii$tick()
+      }
+    }
+    # Save grid
+    purrr::map2(list(pr, vx, vz, exx, ezz, exz, eii, sxx, sxz, sii, ro, nu, nd, mu, ep, et, pr0, prb, dv, tk, cp, kt, ht),
+                c('pr', 'vx', 'vz', 'exx', 'ezz', 'exz', 'eii', 'sxx', 'sxz', 'sii', 'ro', 'nu', 'nd', 'mu', 'ep', 'et', 'pr0', 'prb', 'dv', 'tk', 'cp', 'kt', 'ht'),
+                ~{
+                  rownames(.x) <- gz
+                  colnames(.x) <- gx
+                  .x %>%
+                    as_tibble(rownames = 'z', .name_repair = ~ vctrs::vec_as_names(..., repair = 'unique', quiet = T)) %>%
+                    tidyr::pivot_longer(-z, names_to = 'x', values_to = .y) %>%
+                    mutate('x' = x %>% as.integer(), 'z' = z %>% as.integer())
+                }) %>% 
+      purrr::reduce(left_join, by = c('z', 'x')) %>% 
+      mutate(w = c(0, diff(x)), .before = z)
+  }) %>% 
+    set_names(paste0('grid.', stringr::str_extract(fnames.ordered[prn], 'cd.[0-9]+.[0-9]+')))
+}
+
+# Read .txt files and save rock type grid
+read_rock_nodes <- function(dir, txt = seq(10, 15, 1)){
+  # List txt files
+  fnames <- list.files(dir, '*c[0-9]+.txt', full.names = T)
+  f.order <- fnames %>%
+    purrr::map_int(~.x %>%
+                     stringr::str_extract('[0-9]+.txt+') %>%
+                     stringr::str_extract('[0-9]+') %>%
+                     as.integer()) %>% 
+    order()
+  fnames.ordered <- fnames[f.order]
+  if(max(txt) > length(fnames.ordered)){
+    stop('txt file too large: txts should be between 0 and 100')
+  }
+  # Read binary, save grid
+  grids <- purrr::map(txt, ~{
+    # Filename
+    fname <- fnames.ordered[.x]
+    # Open connection and read file
+    d <- scan(fname)
+    # Time
+    t <- d[1]
+    # x nodes
+    x.coord <- d[2]
+    # z nodes
+    z.coord <- d[3]
+    # rock type
+    r <- d[4:length(d)]
+    # initialize colorgrid
+    colors <- vector('numeric', z.coord*x.coord)
+    # color map
+    c.map <- tibble(
+      type = seq_len(40),
+      r = c(
+        1,
+        0.50588,
+        1,
+        0.68235,
+        1,
+        0.75294,
+        0.50196,
+        0,
+        0,
+        0,
+        0.3,
+        0.14118,
+        0,
+        0.9,
+        0.4,
+        0.8549,
+        0.95294,
+        0.35294,
+        0.1,
+        0,
+        0,
+        0,
+        0,
+        1,
+        1,
+        0.46667,
+        0.50196,
+        0.72549,
+        0.82549,
+        0.6,
+        1,
+        0.99216,
+        0.84706,
+        0.9,
+        0.8,
+        1,
+        0.6,
+        0.6,
+        0.1,
+        0.6
+      ),
+      g = c(
+        1,
+        0.99608,
+        0.50196,
+        0.34118,
+        0.50196,
+        0.75294,
+        0.50196,
+        0.50196,
+        0.84314,
+        0,
+        0.3,
+        0.72157,
+        0.50196,
+        0.4,
+        0,
+        0.59608,
+        0.20392,
+        0.16863,
+        0.6,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0.90196,
+        0.46667,
+        0.50196,
+        0.015686,
+        0.43922,
+        0,
+        0,
+        0.38824,
+        0.078431,
+        0.2,
+        0,
+        0.6,
+        0.4,
+        0.8,
+        0.8,
+        0.5
+      ),
+      b = c(
+        1,
+        0.78824,
+        0,
+        0,
+        0,
+        0.75294,
+        0.50196,
+        0,
+        0,
+        0.71765,
+        0.9,
+        0.99216,
+        1,
+        1,
+        0,
+        0.36078,
+        0.086275,
+        0.027451,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0.31765,
+        0.18824,
+        0.23529,
+        0,
+        0.78431,
+        0.99608,
+        0,
+        0,
+        0.30196,
+        0.15294,
+        0.2,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+      )
+    )
+    num <- 1
+    ind <- 1
+    while(num < length(r)){
+      value <- r[num]
+      if(value == -2){
+        n.color <- r[num+1]
+        mat <- r[num+2]
+        ind_vec <-  seq(ind, ind+n.color-1, 1)
+        ind <- ind+n.color
+        num <- num+3
+      } else {
+        if(value == -1){
+          mat <- NA
+        } else {
+          mat <- value
+        }
+        ind_vec <- ind
+        ind <- ind+1
+        num <- num+1
+      }
+      colors[ind_vec] <- mat
+    }
+    color.grid <- expand.grid(z = seq_len(z.coord), x = seq_len(x.coord)) %>% 
+      tibble() %>% 
+      mutate(type = colors + 1) %>% 
+      filter(type >= 0 && type <= 40) %>% 
+      left_join(c.map, by = 'type')
+  })
 }
--- process.R
+++ process.R
@@ -1,14 +1,33 @@
 source('functions.R')
   
 # Load dataset
+cat('Loading compiled data from "reduced_data.RData"\n')
 load('reduced_data.RData')
 ls.rm <- ls()
 
+# Experiments table
+cat('Saving experiments to tables/experiments.pdf\n')
+mods %>% 
+  select(model, phi, z1100, zc) %>% 
+  kbl(col.names = c('', '$km/100$', '$km$', '$km$'),
+      booktabs = T,
+      longtable = T,
+      caption = 'Experimental results',
+      format = 'latex',
+      escape = F) %>% 
+  add_header_above(c('Experiment', '$\\\\phi/100$', '$z_{1100}$', '$z_c$'),
+                   escape = F,
+                   line = F) %>% 
+  kable_classic() %>% 
+  kable_styling(full_width = F,
+                latex_options = c('repeat_header')) %>% 
+  save_kable(file = 'tables/experiments.pdf')
+
 suppressWarnings({
   # Bivariate regressions phi vs. zc
-  bvm.phi.lin <- map(c('46', '62', '78', '94'),
+  bvm.phi.lin.indv <- map(c('46', '62', '78', '94'),
                      ~m.lin(data = mods %>% filter(z1100==.x), x = 'phi', y = 'zc', type = 'lin', file = paste0('figs/lin_phi_zc_', .x)))
-  
+  bvm.phi.lin <- m.lin(data = mods, x = 'phi', y = 'zc', type = 'lin', plot = T, file = 'figs/lin_phi_zc')
   # Bivariate regressions z1100 vs. zc
   bvm.z1100.lin <- m.lin(data = mods, x = 'z1100', y = 'zc', type = 'lin', anova = T, plot = T, file = 'figs/lin_z1100_zc_')
   bvm.z1100.quad1 <- m.lin(data = mods, x = 'z1100', y = 'zc',  type = 'quad1', file = 'figs/quad1_z1100_zc_')
@@ -28,6 +47,126 @@ segs <- suppressMessages(read_tsv('coupling_data/segments.tsv')) %>%
          zc.quad2 = mvm.quad2$model$estimate[1] + mvm.quad2$model$estimate[2]*z1100 + mvm.quad2$model$estimate[3]*z1100*z1100 + mvm.quad2$model$estimate[4]*phi,
          .before = z1100.ref)
 print(segs)
+
+# Table
+cat('Saving predicted coupling depth results to tables/predicted_zc.pdf\n')
+segs %>% 
+  select(segment, qs, phi, z1100, zc.lin, zc.quad1, zc.quad2) %>% 
+  mutate('phi' = round(phi, 1),
+         'zc.lin' = round(zc.lin),
+         'zc.quad1' = round(zc.quad1),
+         'zc.quad2' = round(zc.quad2),
+         ref = c(1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)) %>% 
+  kbl(col.names = c('', '$mW/m^2$', '$km/100$', '$km$', '$km$', '$km$', '$km$', ''),
+      booktabs = T,
+      caption = 'Predicted coupling depth for modern subduction zone segments',
+      format = 'latex',
+      escape = F) %>% 
+  add_header_above(c('Segment', '$q_s$', '$\\\\phi/100$', '$z_{1100}$', '$z_c^a$', '$z_c^b$', '$z_c^c$', 'Reference^d'),
+                   escape = F,
+                   line = F) %>% 
+  kable_classic() %>% 
+  kable_styling(latex_options = c('scale_down')) %>% 
+  footnote(general = c('$q_s$=average backarc surface heat flow: from Currie & Hyndman (2006)',
+                       '$\\\\phi$=thermal parameter',
+                       '$z_{1100}$=lithospheric thickness'),
+           alphabet = c('Predicted coupling depth using: $z_c=z_{1100}+\\\\phi$',
+                      'Predicted coupling depth using: $z_c=z_{1100}^2+\\\\phi$',
+                      'Predicted coupling depth using: $z_c=z_{1100}+z_{1100}^2+\\\\phi$',
+                      '1=Currie \\\\& Hyndman (2006), 2=Wada \\\\& Wang (2009)'),
+           threeparttable = T,
+           escape = F) %>%
+  save_kable(file = 'tables/predicted_zc.pdf')
+
+# ANOVA table
+cat('Saving ANOVA results to tables/anova.pdf\n')
+bvm.z1100.lin$anova %>% 
+  select(-null.value) %>% 
+  mutate('estimate' = round(estimate, 1),
+         'conf.low' = round(conf.low, 1),
+         'conf.high' = round(conf.high, 1),
+         'adj.p.value' = scientific(adj.p.value, digits = 3),
+         'term' = '$z_{1100}$') %>% 
+  kbl(col.names = c('', '', '$km$', '$km$', '$km$', ''),
+      booktabs = T,
+      caption = 'Summary of ANOVA test',
+      format = 'latex',
+      escape = F) %>% 
+  add_header_above(c('Term', 'Contrast', 'Estimate', 'Upper Bound', 'Lower Bound', 'p value'),
+                   escape = F,
+                   line = F) %>% 
+  kable_classic() %>% 
+  kable_styling(latex_options = c('scale_down')) %>% 
+  footnote(general = c("Pair-wise Tukey's test comparing means between groups",
+                       'Estimates are differences between means',
+                       'Null hypothesis is that means are not different'),
+           threeparttable = T,
+           escape = F) %>%
+  save_kable(file = 'tables/anova.pdf')
+
+# Regression table
+cat('Saving regression results to tables/regression.pdf\ntables/reg_fits.pdf\n')
+map_df(list(bvm.phi.lin,
+         bvm.z1100.lin,
+         bvm.z1100.quad1,
+         bvm.z1100.quad2,
+         mvm.lin,
+         mvm.quad1,
+         mvm.quad2),
+    ~.x$model, .id = 'reg') %>% 
+  select(-statistic) %>% 
+  mutate('estimate' = round(estimate, 1),
+         'std.error' = round(std.error, 1),
+         'p.value' = scientific(p.value, digits = 3),
+         'term' = ifelse(term == 'phi', '$\\phi$',
+                         ifelse(term == 'z1100', '$z_{1100}',
+                                ifelse(term == 'z1100^2', 'z_{1100}^2', term)))) %>% 
+  kbl(col.names = c('Model', 'Term', 'Estimate', 'Std. Error', 'p value'),
+      booktabs = T,
+      caption = 'Summary of regression models',
+      format = 'latex',
+      escape = F) %>% 
+  kable_classic() %>%
+  footnote(number = c('$z_c=\\\\phi$',
+                      '$z_c=z_{1100}$',
+                      '$z_c=z_{1100}^2$',
+                      '$z_c=z_{1100}+z_{1100}^2$',
+                      '$z_c=z_{1100}+\\\\phi$',
+                      '$z_c=z_{1100}^2+\\\\phi$',
+                      '$z_c=z_{1100}+z_{1100}^2+\\\\phi$'),
+           threeparttable = T,
+           escape = F) %>%
+  save_kable(file = 'tables/regression.pdf')
+
+map_df(list(bvm.phi.lin,
+            bvm.z1100.lin,
+            bvm.z1100.quad1,
+            bvm.z1100.quad2,
+            mvm.lin,
+            mvm.quad1,
+            mvm.quad2),
+       ~.x$fit, .id = 'reg') %>% 
+  select(reg, r.squared, p.value, AIC) %>% 
+  mutate('r.squared' = round(r.squared, 3),
+         'p.value' = scientific(p.value, digits = 3),
+         'AIC' = round(AIC)) %>% 
+  kbl(col.names = c('Model', '$R^2$', 'p value', 'AIC^a'),
+      booktabs = T,
+      caption = 'Summary of regression model fits',
+      format = 'latex',
+      escape = F) %>% 
+  kable_classic() %>%
+  footnote(alphabet = 'AIC = Akaike Information Criterion',
+           number = c('$z_c=\\\\phi$',
+                      '$z_c=z_{1100}$',
+                      '$z_c=z_{1100}^2$',
+                      '$z_c=z_{1100}+z_{1100}^2$',
+                      '$z_c=z_{1100}+\\\\phi$',
+                      '$z_c=z_{1100}^2+\\\\phi$',
+                      '$z_c=z_{1100}+z_{1100}^2+\\\\phi$'),
+           threeparttable = T,
+           escape = F) %>%
+  save_kable(file = 'tables/reg_fits.pdf')
 
 # Clean up environment
 rm(list = ls.rm)
